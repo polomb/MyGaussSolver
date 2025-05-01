d5b357b6b4ca05d15b6929ab5fda85cf1c28522a
Polomb1 <20060711roma@mail.ru>
2025-05-01 15:09:27 +0300

    Add files via upload

--- Matrix.h
+++ Matrix.h
@@ -1,4 +1,4 @@
-#ifndef _MY_MATRIX_
+ï»¿#ifndef _MY_MATRIX_
 #define _MY_MATRIX_
 
 #include "Vector.h"
@@ -7,79 +7,83 @@ class Matrix
 {
 private:
 	int y = 0; int x = 0;
-	Vector* matrix;
-	Vector atrix;
+	Vector matrix;
 public:
-	Matrix(){}
+	Matrix() {}
 	Matrix(int n, int m)
 	{
 		y = n; x = m;
-		matrix = new Vector [n];
-		for (int i = 0; i < n; i++)
-			matrix[i].obnulenie(m);
+		matrix.obnulenie(n * m);
 	}
 	double& operator()(int i, int j)
 	{
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
 		if (i >= y || j >= x)
 			std::cout << "Error: MatrixOperator()";
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
-		//Âåðíóòü A[i,j]
-		return matrix[i][j];
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
+		//Ð’ÐµÑ€Ð½ÑƒÑ‚ÑŒ A[i,j]
+		return matrix[i * y + j];
 	}
 	double operator()(int i, int j) const
 	{
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
 		if (i >= y || j >= x)
 			std::cout << "Error: MatrixOperator()";
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
-		//Âåðíóòü A[i,j]
-		return matrix[i][j];
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
+		//Ð’ÐµÑ€Ð½ÑƒÑ‚ÑŒ A[i,j]
+		return matrix[i * y + j];
 	}
 	double MaxRow(int j)
 	{
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
 		if (j >= x)
 			std::cout << "Error: MatrixMaxRow";
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
-		double MaxRow = matrix[0][j];
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
+		double MaxRow = matrix[j];
 		for (int i = 0; i < y; i++)
 		{
-			MaxRow = std::max(MaxRow, matrix[i][j]);
+			MaxRow = std::max(MaxRow, matrix[i * y + j]);
 		}
 		return MaxRow;
 	}
 	double MaxLine(int i)
 	{
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
 		if (i >= y)
 			std::cout << "Error: MatrixMaxLine";
-		//ÏÎÒÎÌ ÓÁÐÀÒÜ
-		double MaxLine = matrix[i][0];
+		//ÐŸÐžÐ¢ÐžÐœ Ð£Ð”ÐÐ›Ð˜Ð¢Ð¬
+		double MaxLine = matrix[i * y];
 		for (int j = 0; j < x; j++)
 		{
-			MaxLine = std::max(MaxLine, matrix[i][j]);
+			MaxLine = std::max(MaxLine, matrix[i * y + j]);
 		}
 		return MaxLine;
 	}
 	void swapLine(int y1, int y2, int x)
 	{
+		Vector temp(x);
 		for (int j = 0; j < x; j++)
 		{
-			double temp = matrix[y1][j];;
-			matrix[y1][j] = matrix[y2][j];
-			matrix[y2][j] = temp;
+			temp[j] = matrix[y1 * x + j];
+		}
+		for (int j = 0; j < x; j++)
+		{
+			matrix[y1 * x + j] = matrix[y2 * x + j];
+		}
+		for (int j = 0; j < x; j++)
+		{
+			matrix[y2 * x + j] = temp[j];
 		}
 	}
-	void print() const 
+	void print() const
 	{
 		for (int i = 0; i < y; i++)
 		{
 			for (int j = 0; j < x; j++)
-				std::cout << matrix[i][j] << " ";
+				std::cout << matrix[y * i + j] << " ";
 			std::cout << std::endl;
 		}
-    }
+	}
 };
 
 #endif
